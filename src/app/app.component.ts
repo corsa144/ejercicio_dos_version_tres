2ec310d45943ffa141c17e00b88035c24f1ac295
corsa144 <corsa144@gmail.com>
2024-04-22 18:56:10 -0300

    Ejercicio 2, login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,64 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { BienvenidaComponent } from './component/bienvenida/bienvenida.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    BienvenidaComponent,
+    RouterLink
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'ejercicio_dos_version_tres';
+export class AppComponent implements OnInit{
+  isModalVisible: boolean = false;
+  selectedTab: string = 'login';
+  loginEmail: string = '';
+  loginPassword: string = '';
+  rememberMe: boolean = false;
+  signupUsername: string = '';
+  signupEmail: string = '';
+  signupPassword: string = '';
+  acceptTerms: boolean = false;
+  resetEmail: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  toggleModal(tab: string): void {
+    this.isModalVisible = !this.isModalVisible;
+    if (this.isModalVisible) {
+      this.selectedTab = tab;
+    }
+  }
+
+  closeModal(): void {
+    this.isModalVisible = false;
+  }
+
+  selectTab(tab: string): void {
+    this.selectedTab = tab;
+  }
+
+  login(): void {
+    console.log('Login');
+    
+  }
+
+  signup(): void {
+    console.log('Sign up');
+    
+  }
+
+  resetPassword(): void {
+    console.log('Reset password');
+    
+  }
+
+
 }
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { BienvenidaComponent } from './component/bienvenida/bienvenida.component';
+import { LoginComponent } from './component/login/login.component';
+import { ErrorComponent } from './component/error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "bienvenida",
+        component: BienvenidaComponent,
+    },
+    {
+        path: "login",
+        component: LoginComponent,
+    },
+    {
+        path: "bienvenido",
+        redirectTo: "bienvenida",
+    },
+    {
+        path: "",
+        component: LoginComponent,
+    },
+    {
+        path: "**",
+        component: ErrorComponent,
+    }
+];
